074d1e05d753580ddb30ee7018eaa1a6167d79fa
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-04-13 03:51:52 +1000

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -2,7 +2,8 @@
 # Copyright 2019 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
+#
+# You may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #      http://www.apache.org/licenses/LICENSE-2.0
@@ -17,14 +18,22 @@
 # This contains the module build definitions for the hardware-specific
 # components for this device.
 #
-# As much as possible, those components should be built unconditionally,
-# with device-specific names to avoid collisions, to avoid device-specific
-# bitrot and build breakages. Building a component unconditionally does
-# *not* include it on all devices, so it is safe even with hardware-specific
-# components.
+# Device-specific hardware modules should be built unconditionally
+# to avoid device-specific bitrot or build breakages.
 
 LOCAL_PATH := $(call my-dir)
 
+# Only build for gts4lvwifi
 ifneq ($(filter gts4lvwifi,$(TARGET_DEVICE)),)
+
+# Include all makefiles under this directory recursively
+# Android 11 supports the same call structure
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+# Optional: define VNDK-specific modules if required
+# Example:
+# ifeq ($(TARGET_USE_VNDK),true)
+#     include $(LOCAL_PATH)/vndk_modules.mk
+# endif
+
 endif
